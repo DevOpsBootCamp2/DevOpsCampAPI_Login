a60251cec946902661e9f4d32c8a42c332de59c8
Harvey1327 <harvey_bal@hotmail.co.uk>
2017-04-04 14:40:59 +0100

    Added Junit

---
+++ src/test/java/com/cap/domain/TokenTest.java
@@ -0,0 +1,18 @@
+package com.cap.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TokenTest {
+
+	@Test
+	public void testTokenIsConstructed(){
+		Token token = new Token();
+		token.setEmail("email");
+		token.setPassword("password");
+		
+		assertEquals("email", token.getEmail());
+		assertEquals("password",token.getPassword());
+	}
+}
